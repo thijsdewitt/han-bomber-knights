1b40c6aa9cde658a01489dd30de840a309eb2343
Thijs de Witt <74593418+thijsdewitt@users.noreply.github.com>
2025-03-28 13:11:17 +0100

    Remove unnecessary constructor

--- src/main/java/nl/thijsdewitt/han_bomber_knights/entities/map/Image.java
+++ src/main/java/nl/thijsdewitt/han_bomber_knights/entities/map/Image.java
@@ -1,14 +1,6 @@
 package nl.thijsdewitt.han_bomber_knights.entities.map;
 
-import org.jetbrains.annotations.NotNull;
-
 public record Image(int width, int height, int[] pixels) {
-    public Image(int width, int height, int[] pixels) {
-        this.width = width;
-        this.height = height;
-        this.pixels = pixels;
-    }
-
     public int[][] getPixels2D() {
         int[][] pixel2D = new int[width][height];
         for (int x = 0; x < width; x++) {
