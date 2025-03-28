59230509460cc8014a909217b9c54dce54142b9f
Thijs de Witt <74593418+thijsdewitt@users.noreply.github.com>
2025-03-28 15:31:01 +0100

    Add player implementation

--- src/main/java/nl/thijsdewitt/han_bomber_knights/entities/Player.java
+++ src/main/java/nl/thijsdewitt/han_bomber_knights/entities/Player.java
@@ -1,23 +1,31 @@
 package nl.thijsdewitt.han_bomber_knights.entities;
 
 import com.github.hanyaeger.api.Coordinate2D;
+import com.github.hanyaeger.api.Size;
 import com.github.hanyaeger.api.entities.Collided;
 import com.github.hanyaeger.api.entities.Collider;
+import com.github.hanyaeger.api.entities.Direction;
 import com.github.hanyaeger.api.entities.impl.DynamicSpriteEntity;
+import com.github.hanyaeger.api.userinput.KeyListener;
+import javafx.scene.input.KeyCode;
 import nl.thijsdewitt.han_bomber_knights.entities.powerups.AbstractPowerUp;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
-public class Player extends DynamicSpriteEntity implements Collided, Collider {
+public class Player extends DynamicSpriteEntity implements Collided, Collider, KeyListener {
     private static final int MAX_HEALTH = 3;
 
     private final ArrayList<AbstractPowerUp> powerUps = new ArrayList<>();
     private int health = 3;
     private int explosionRadius = 3;
+    private int walkSpeed = 3;
 
-    protected Player(String resource, Coordinate2D initialLocation) {
-        super(resource, initialLocation);
+    public Player(Coordinate2D location) {
+        super("sprites/Blue Knight run Sprite-sheet 16x17.png", location, new Size(64), 4, 8);
+        setAutoCycle(1000 / 8);
+        setAutoCycleRow(0);
     }
 
     @Override
@@ -51,5 +59,32 @@ public class Player extends DynamicSpriteEntity implements Collided, Collider {
 
     public void setExplosionRadius(int explosionRadius) {
         this.explosionRadius = explosionRadius;
+    }
+
+    @Override
+    public void onPressedKeysChange(Set<KeyCode> pressedKeys) {
+        if (pressedKeys.contains(KeyCode.UP)) {
+            setMotion(walkSpeed, Direction.UP);
+            setAutoCycleRow(2);
+        } else if (pressedKeys.contains(KeyCode.DOWN)) {
+            setMotion(walkSpeed, Direction.DOWN);
+            setAutoCycleRow(0);
+        } else if (pressedKeys.contains(KeyCode.LEFT)) {
+            setMotion(walkSpeed, Direction.LEFT);
+            setAutoCycleRow(3);
+        } else if (pressedKeys.contains(KeyCode.RIGHT)) {
+            setMotion(walkSpeed, Direction.RIGHT);
+            setAutoCycleRow(1);
+        } else {
+            setSpeed(0);
+        }
+    }
+
+    public int getWalkSpeed() {
+        return walkSpeed;
+    }
+
+    public void setWalkSpeed(int walkSpeed) {
+        this.walkSpeed = walkSpeed;
     }
 }
